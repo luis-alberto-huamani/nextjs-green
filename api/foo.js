92c415c860dbfef91097ea14a570a32a59c5b11f
necr0sys <felixtineo05@gmail.com>
2019-07-05 12:09:48 -0400

    fixing static

--- api/foo.js
+++ api/foo.js
@@ -3,7 +3,7 @@ const multer = require('multer');
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'static')
+    cb(null, '../static')
   },
   filename: function (req, file, cb) {
     cb(null, file.fieldname + '-' + Date.now())

547a3577876d2f7fe3589cd057c4967eb6667a8a
necr0sys <felixtineo05@gmail.com>
2019-07-06 17:06:41 -0400

    test3

--- api/middleware/common.js
+++ api/middleware/common.js
@@ -4,6 +4,8 @@ const compression = require('compression');
 const cors = require('cors');
 const dotenv = require('dotenv');
 //{"src":"package.json","use":"@now/static-build"},
+/*    "now-build": "next build && next export -o dist",
+    "now-dev": "next -p $PORT"*/
 module.exports = [
   dotenv.config(),
   helmet(),

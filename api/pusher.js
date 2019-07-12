0c8613b1af51055318cb475e73a3341956bde454
necr0sys <felixtineo05@gmail.com>
2019-07-12 05:24:11 -0400

    commment

--- api/pusher.js
+++ api/pusher.js
@@ -1,16 +1,16 @@
 const app = require('./util/app');
-const Pusher = require('pusher');
+//const Pusher = require('pusher');
 
-var pusher = new Pusher({
+/*var pusher = new Pusher({
   appId: '821952',
   key: 'bfb62abbe03a8b1e8c89',
   secret: '545cc02b18a4fa944315',
   cluster: 'us2',
   encrypted: true
-});
+});*/
 
 app.get('*', (req, res) => {
-  pusher.trigger('push', 'my-push', {"message": "hello world"});
+  //pusher.trigger('push', 'my-push', {"message": "hello world"});
   res.status(200).send('hey');
 })
 

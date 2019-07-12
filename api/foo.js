1be94e6377bea8f51218c197b57936a15ff72bba
necr0sys <felixtineo05@gmail.com>
2019-07-12 08:13:29 -0400

    test4

--- api/foo.js
+++ api/foo.js
@@ -1,2 +1,9 @@
-module.exports = ('*', (req, res) => res.send("hola mundo"));
-//"now-build": "next build"
+const app = require('./util/app');
+
+app.get('*', (req, res) => {
+  const name = req.query;
+  res.status(200).send(`bienvenido ${name}`);
+});
+
+module.exports = app;
+

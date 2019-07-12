af75d2a9798429dd3845efbe301d280cdb2c2d19
necr0sys <felixtineo05@gmail.com>
2019-07-12 07:54:03 -0400

    deploy

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2d0a5c28e720ea3be378";
+/******/ 	var hotCurrentHash = "e8fd612357d2247dfc07";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ api/foo.js
@@ -0,0 +1,2 @@
+module.exports = ('*', (req, res) => res.send("hola mundo"));
+//"now-build": "next build"

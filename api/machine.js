14d6898f550a9cbfa7696ad42bc656f276f31fd4
necr0sys <felixtineo05@gmail.com>
2019-07-15 17:10:33 -0400

    test machine

---
+++ api/machine.js
@@ -0,0 +1,18 @@
+const app = require('./util/app');
+const mongooseConnect = require('./util/mongoose');
+const UserSchema = require('../models/user');
+
+
+[...mongooseConnect];
+
+
+app.post('*', async (req, res) => {
+  const { productId, userId, precio  } = req.body;
+  res.status(200).send(`la transaccion del producto ${productId} con precio ${precio} del usuario ${userId} ha sido completada exitosamente`);
+});
+
+
+//app.listen(4000, console.log('server on'));
+module.exports = app;
+
+//curl -d '{"productId": "123456", "userId": "789456", "precio": "500gc"}' -H "Content-Type: application/json" -X POST https://nextjs-green.now.sh/api/machine.ks
